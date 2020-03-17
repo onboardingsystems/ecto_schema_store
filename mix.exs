ac0ca5b7ceab998863c2e0f3af886238d9d3d6e0
Brett Valantine <brettv@onboardingsystems.com>
2020-03-17 16:27:57 -0600

    update dialyzer and making it pass

--- mix.exs
+++ mix.exs
@@ -4,25 +4,37 @@ defmodule EctoStore.Mixfile do
   @version "3.0.0"
 
   def project do
-    [app: :ecto_schema_store,
-     version: @version,
-     elixir: "~> 1.6",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     compilers: [:gettext] ++ Mix.compilers,
-     deps: deps(),
-     description: "Builds upon Ecto to create a ready to go customizable CRUD module for a schema.",
-     name: "Ecto Schema Store",
-     package: %{
-       licenses: ["Apache 2.0"],
-       maintainers: ["Joseph Lindley"],
-       links: %{"GitHub" => "https://github.com/cenurv/ecto_schema_store"},
-       files: ~w(mix.exs README.md CHANGELOG.md lib)
-     },
-     docs: [source_ref: "v#{@version}", main: "readme",
-            canonical: "http://hexdocs.pm/ecto_schema_store",
-            source_url: "https://github.com/cenurv/ecto_schema_store",
-            extras: ["CHANGELOG.md", "README.md"]]]
+    [
+      app: :ecto_schema_store,
+      version: @version,
+      elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      compilers: [:gettext] ++ Mix.compilers(),
+      deps: deps(),
+      description:
+        "Builds upon Ecto to create a ready to go customizable CRUD module for a schema.",
+      name: "Ecto Schema Store",
+      package: %{
+        licenses: ["Apache 2.0"],
+        maintainers: ["Joseph Lindley"],
+        links: %{"GitHub" => "https://github.com/cenurv/ecto_schema_store"},
+        files: ~w(mix.exs README.md CHANGELOG.md lib)
+      },
+      dialyzer: [
+        plt_add_deps: :transitive,
+        # ignore_warnings: "./.dialyzer_ignore.exs",
+        plt_add_apps: [:ecto, :elixir, :event_queues],
+        flags: [:error_handling, :race_conditions, :unknown, :underspecs]
+      ],
+      docs: [
+        source_ref: "v#{@version}",
+        main: "readme",
+        canonical: "http://hexdocs.pm/ecto_schema_store",
+        source_url: "https://github.com/cenurv/ecto_schema_store",
+        extras: ["CHANGELOG.md", "README.md"]
+      ]
+    ]
   end
 
   # Configuration for the OTP application
@@ -42,10 +54,13 @@ defmodule EctoStore.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ecto, "~> 3.0"},
-     {:ecto_sql, "~> 3.0"},
-     {:ex_doc, "~> 0.19", only: [:docs, :dev]},
-     {:gettext, "~> 0.15"},
-     {:event_queues, "~> 3.0", optional: true}]
+    [
+      {:ecto, "~> 3.0"},
+      {:ecto_sql, "~> 3.0"},
+      {:ex_doc, "~> 0.19", only: [:docs, :dev]},
+      {:gettext, "~> 0.15"},
+      {:event_queues, "~> 3.0", optional: true},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+    ]
   end
 end
